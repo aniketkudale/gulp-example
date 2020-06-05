ddc9b040a860d67d73cad969d9f006f8ee890c1a
Aniket Kudale <aniket.kudale@hotmail.com>
2020-06-05 14:58:12 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,23 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	sourcemaps = require('gulp-sourcemaps')
 	sass = require('gulp-sass');
-	
+
+//Task for jsHint	
 gulp.task('jshint', function() {
 	return gulp.src('js/*.js')
 			   .pipe(jshint())
 			   .pipe(jshint.reporter('jshint-stylish'));
 });
 
+//Task to convert SASS to CSS
 gulp.task('build-sass', function() {
 	return gulp.src('sass/*.scss')
 			   .pipe(sass())
 			   .pipe(concat('style.css'))
 			   .pipe(gulp.dest('dist/css'));
 });
+
+//Task to create bundle.js
 gulp.task('build-js', function() {
 	return gulp.src('js/*.js')
 			   .pipe(sourcemaps.init())
@@ -26,12 +30,13 @@ gulp.task('build-js', function() {
 			   .pipe(gulp.dest('dist/js'));
 });
 
+//Watch for changes
 gulp.task('watch', function() {
 	gulp.watch('test/*.js', gulp.series('jshint'));
 	gulp.watch('sass/*.scss', gulp.series('build-sass'));
 	gulp.watch('js/*.js', gulp.series('build-js'));
 });
 
-// create a default task and just log a message
+// create a default task for watch
 gulp.task('default', gulp.series('watch'));
 	
